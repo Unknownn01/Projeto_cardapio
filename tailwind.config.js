16f9c14e60c9680da9d411d6e3da6949883287df
Unknownn01 <lopesg13@gmail.com>
2024-09-13 12:08:25 -0400

    opçoes de contato

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    fontFamily:{
+      'sans': ['Roboto']
+    },
     extend: {
       backgroundImage:{
         "home": "url('/assets/logo.png')"
